98902080de343a6bab38f25dfa2cba7ed65a8494
Maxepe <maxipetrei@outlook.com>
2022-08-20 14:09:50 -0300

    nkkridev-2
    Commit para seguir. Aux.

--- src/main/resources/static/js/consultarUsuario.js
+++ src/main/resources/static/js/consultarUsuario.js
@@ -9,5 +9,13 @@ async function consultarUsuario() {
     });
 
     const response = await request.json()
+    console.log(response.nombreUsuario)
+    let tr = $('<tr/>');
+    tr.append("<td>" + '' + "</td>");
+    tr.append("<td>" + '' + "</td>");
+    tr.append("<td>" + '' + "</td>")
+    // result["response"].parameter_name
     console.log(response)
+    result["response"].status
+    $('#tablaUsuarios tbody').append(tr);
 }
